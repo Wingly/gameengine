488d681c4403e6c7aec64c334bd5ef162cd7e217
Erik/Wingly <Erik_the_great_one@hotmail.com>
2014-09-10 10:23:13 +0200

    It's alive(utropstecken)

--- engine/Include/MemPool.h
+++ engine/Include/MemPool.h
@@ -5,10 +5,9 @@ class MemPool
 {
 private:
 	// Startpointer
-	void* m_start;
+	uint64_t* m_start;
 
-	void* m_firstFree;
-	//void* m_lastFree;
+	uint64_t* m_firstFree;
 
 	unsigned int m_numBlocks;
 	unsigned int m_sizeOfBlock;
@@ -20,17 +19,17 @@ public:
 		m_numBlocks = p_numBlocks;
 		m_sizeOfBlock = sizeof(T);
 
-		m_start = malloc(p_numBlocks * m_sizeOfBlock);
+		m_start = (uint64_t*)malloc(p_numBlocks * m_sizeOfBlock);
 		m_firstFree = m_start;
-		//m_lastFree = nullptr;
-
-		char** local = (char**)m_firstFree;
+		
+		uint64_t* currentBlock = m_firstFree;
 		for(unsigned int i = 0; i < p_numBlocks-1; i++)
 		{
-			*local = local + m_sizeOfBlock;
-			local = local + m_sizeOfBlock;
+			uint64_t adress = (uint64_t)currentBlock; //get pointer adress as uint
+			*currentBlock = (uint64_t)(adress + m_sizeOfBlock); //Save next block as uint in the currentBlock
+			currentBlock = reinterpret_cast<uint64_t*>(*currentBlock); //Set currentBlock to next block		
 		}
-		*local = nullptr;
+		currentBlock = nullptr;
 	}
 
 	~MemPool()
@@ -44,8 +43,8 @@ public:
 			//Inget ledigt minne!
 		}
 		T* local = (T*)m_firstFree;
-		m_firstFree = *m_firstFree;
 
+		m_firstFree = reinterpret_cast<uint64_t*>(*m_firstFree);
 		return local;
 	}
 };
