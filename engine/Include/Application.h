f6c56c0284da041954f826663278a60d1eb5c912
Poelsa <joel.begnert@gmail.com>
2014-09-18 15:29:52 +0200

    Started with thread setup

--- engine/Include/Application.h
+++ engine/Include/Application.h
@@ -1,15 +1,33 @@
 #pragma once
+
+#include <engine/Include/MemoryAllocator.h>
+struct Particle {
+	float lifeTime;
+	float startSpeed;
+	float startX;
+	float startY;
+	float startZ;
+	float dirX;
+	float dirY;
+	float dirZ;
+	float colR;
+	float colG;
+	float colB;
+};
 class MemoryAllocator;
 class Application
 {
 public:
 	Application();
-	int Run();
 	~Application();
+
+	void ThreadInit();
+	void ThreadRun();
+	int Run();
 
 private:
 	void ThreadFunc(void* p_init);
-
+	MemPool<Particle>* m_pool;
 	bool m_running;
 
 };
--- engine/Source/Application.cpp
+++ engine/Source/Application.cpp
@@ -1,7 +1,14 @@
 #include <engine/Include/Application.h>
-#include <engine/Include/MemoryAllocator.h>
+
 #include <thread>
 #include <stdio.h>
+
+static const unsigned int NUM_THREADS = 4;
+static const unsigned int NUM_BLOCKS = 1024;
+static const unsigned int NUM_PARTICLES_PER_THREAD = NUM_BLOCKS/NUM_THREADS;
+
+static const unsigned int RUN_PARTICLE_TEST = true;
+
 
 enum class StopCode
 {
@@ -10,24 +17,15 @@ enum class StopCode
 	CrashStop
 };
 
-struct Particle {
-	float lifeTime;
-	float startSpeed;
-	float startX;
-	float startY;
-	float startZ;
-	float dirX;
-	float dirY;
-	float dirZ;
-	float colR;
-	float colG;
-	float colB;
-};
+
 
 Application::Application()
 {
+	
+
 	MemoryAllocator* memAl = new MemoryAllocator();
 	MemPool<float>* a = memAl->CreatePool<float>(5, 16);
+	m_pool = memAl->CreatePool<Particle>(NUM_BLOCKS, 16);
 	
 	float* b = a->getBlock();
 	*b = 1.0f;
@@ -53,12 +51,34 @@ Application::~Application()
 	
 }
 
+void Application::ThreadInit()
+{
+	if(RUN_PARTICLE_TEST)
+	{
+		Particle* particles[NUM_PARTICLES_PER_THREAD];// = new Particle[];
+		for(int i = 0; i < NUM_PARTICLES_PER_THREAD; i++) 
+		{
+			particles[i] = m_pool->getBlock();
+		}
+		
+	}
+	else
+	{
+
+	}
+}
+
+void Application::ThreadRun()
+{
+
+}
+
 
 int Application::Run()
 {
 	while(m_running)
 	{
-		// do stuff
+		ThreadRun();
 	}
 
 	return (int)StopCode::CleanStop;
